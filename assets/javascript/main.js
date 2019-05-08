ea0ac4104d382bfaee8576629ad707cb384d370d
Ryan Creveling <ryancreveling@gmail.com>
2019-05-08 11:52:19 -0400

    Finally got JavaScript to listen for changes in Data in Firebase, can now have two different windows open and have different users input information and update it live-ish

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
         display: "none"
     })
 });
+var userpick;
+var userpick2;
 
 var firebaseConfig = {
     apiKey: "AIzaSyCRPSJdpNXRVd8rCKRyhqE6aIqQJ3pQ3IY",
@@ -19,6 +21,29 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
+function reloaded() {
+    database.ref("Player 1").once("value", function (snapshot) {
+        var usernameOne = snapshot.val().username
+        console.log(usernameOne)
+        $("#p1Username").text(usernameOne)
+    })
+    database.ref("Player 2").once("value", function (snapshot) {
+        var usernameTwo = snapshot.val().username
+        console.log(usernameTwo)
+        $("#p2Username").text(usernameTwo)
+    })
+}
+reloaded();
+
+database.ref("Player 1").on("child_changed", function (snapshot) {
+    var newName = snapshot.val().username
+    $("#p1Username").text(newName)
+})
+database.ref("Player 1").on("child_changed", function (snapshot) {
+    var newName = snapshot.val().username
+    $("#p2Username").text(newName)
+})
+
 $("#submit").one("click", function (event) {
     event.preventDefault();
     console.log()
@@ -26,61 +51,71 @@ $("#submit").one("click", function (event) {
         var username = $("#username").val().trim();
         var userImage = $("#userImagePath").val().trim();
         var tagline = $("#tagline").val().trim()
-        var userpick = $(".selected").text();
+        userpick = $(".selected").text();
         console.log(username, userImage, tagline, userpick)
-        database.ref(userpick).push({
+        database.ref("Player 1").set({
             username: username,
             userImage: userImage,
             tagline: tagline,
-            player: userpick,
+            submitted: "true",
+
         })
         $("#loadCont").css({
             display: "none",
         })
-        $("#mainWrap").css({
-            display: "block"
-        })
+
+        $("#p1Username").text(username)
 
     } else {
 
         var username2 = $("#username").val().trim();
         var userImage2 = $("#userImagePath").val().trim();
         var tagline2 = $("#tagline").val().trim()
-        var userpick2 = $(".selected").text();
+        userpick2 = $(".selected").text();
         console.log(username2, userImage2, tagline2, userpick2)
-        database.ref(userpick2).push({
-            username2: username2,
-            userImage2: userImage2,
-            tagline2: tagline2,
-            player2: userpick2,
+        database.ref("Player 2").set({
+            username: username2,
+            userImage: userImage2,
+            tagline: tagline2,
+            submitted: "true",
+
         })
         $("#loadCont").css({
             display: "none",
+        })
+
+        $("#p2Username").text(username2)
+    }
+    playerSet();
+})
+
+
+function playerSet() {
+    var p1submit = "";
+    var p2submit = "";
+    var p1tagline;
+    var p2tagline;
+    database.ref("Player 1").on("value", function (snapshot) {
+        p1tagline = snapshot.val().tagline
+        p1submit = snapshot.val().submitted
+    })
+    database.ref("Player 2").on("value", function (snapshot) {
+        p2tagline = snapshot.val().tagline
+        p2submit = snapshot.val().submitted
+    })
+    console.log(p1submit, p2submit)
+    if (p1submit === "true" && p2submit === "true") {
+        $("#p1tagline").text('"' + p1tagline + '"')
+        $("#p2tagline").text('"' + p2tagline + '"')
+        $("h5").css({
+            fontFamily: "Georgia, serif",
+            fontSize: "1.5em",
+            textAlign: "center"
         })
         $("#mainWrap").css({
             display: "block"
         })
     }
-    playerSet();
-})
-function player1set() {
-    database.ref("Player 1").on("child_added", function (snapshot) {
-
-    })
-}
-function player2set() {
-    database.ref("Player 2").on("child_added", function (snapshot) {
-        $("#p2Username").text(snapshot.val().username2)
-        console.log(snapshot.val())
-    })
-
-}
-
-function playerSet(snapshot) {
-    var playerOne = snapshot.ref("Player 1").val().username
-    var playerTwo = snapshot.ref("Player 2").val().username2
-    $("#p1Username").text(playerOne)
-    $("#p2Username").text(playerTwo)
 }
 
 
